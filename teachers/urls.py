aa211ee1602054256af63e85cba33bd1b409e2a4
Samandar0005 <swatushuwu@gmail.com>
2024-12-04 22:56:51 +0300

    done

---
+++ teachers/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'teachers'
+
+urlpatterns = [
+    path('', views.teacher_list, name='teacher_list'),
+    path('create/', views.teacher_form, name='teacher_form'),
+    path('detile/<int:pk>/', views.teacher_detail, name='teacher_detail'),
+    path('delete/<int:pk>/', views.teacher_delete, name='teacher_delete'),
+]
+
